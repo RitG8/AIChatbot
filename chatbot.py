521132e7ae6ec6ed976ba554e872bd616c0fd7f9
Ritika Gurjar <ritgurjar@gmail.com>
2023-12-15 13:22:22 -0500

    first commit

---
+++ chatbot.py
@@ -0,0 +1,72 @@
+import random
+import csv
+
+def response(text):
+
+    if text.find("Northeastern") >= 0:
+        return "I noticed you mentioned Northeastern. I graduated from there in 1990!"
+    
+    file = read_csv("responses.csv")
+    text = words_in_text(text)
+    line_num = find_best_line(text, file)
+
+    if line_num == -1:
+        return "I don't have the vocabulary yet to respond to that. Train me on a response!"
+    
+    else:
+        line = file[line_num]
+        return line[random.randint(1,len(line)-1)]
+    
+#reads the file and returns a list where each element represents a row of a line.    
+def read_csv(file):
+    with open(file, newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=';')
+        all_lines = []
+        for line in reader:
+            all_lines += [line]
+        return all_lines
+    
+#formats the string to return only the unique words
+def words_in_text(text):
+    text = text.split()
+
+    #removes everything in the string unless it is an alphabetical character
+    for i in range(len(text)):
+        alpha_word = ""
+        for char in text[i]:
+            if char.isalpha():
+                alpha_word += char
+        alpha_word = alpha_word.lower()
+        text[i] = alpha_word
+    
+    #removes common words
+    return set(text)
+
+#scan thru each line excluding the header
+#format the string accordingly
+#then keep track of the line number that had the most intersections
+#takes in a formatted text input and a formatted file input
+#returns the line number of the file that best matches the input text.
+def find_best_line(text, file):
+    line_num = -1
+    max_num = 0
+
+    for i in range(1, len(file)):
+        question = file[i][0]
+        question = words_in_text(question)
+        num_intersect = len(text & question)
+
+        if max_num < num_intersect:
+            line_num = i
+            max_num = num_intersect
+    
+    return line_num
+
+#takes in an array of user text where the first thing is a question and rest are answers
+def add_to_csv(text):
+    with open('responses.csv', 'a', newline='') as csvfile:
+            writer = csv.writer(csvfile, delimiter=';')
+            print(text)
+            writer.writerow(text)
+
+
---
+++ interface_chatbot.py
@@ -0,0 +1,211 @@
+import pygame
+import chatbot
+import os
+
+pygame.init()
+colors = {"fire engine red": "#db162f", "space cadet":"#1F2041", "english violet":"#4B3F72", "bright pink":"#FB6376", "melon":"#FCB1A6", "pale dogwood":"#FFDCCC",
+          "viridian":"#53917E", "fire brick": "#AD2E24", "castleton green": "#226752"}
+
+
+def run_chatbot():
+    screen = pygame.display.set_mode([500, 500])
+    running = True
+
+    base_font = pygame.font.Font(None, 32) 
+    user_text = ''
+    text_box_clicked = False
+    response = "Hello. How may I help you today?"
+    spoke = False
+    time = 0
+
+    while running:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+            if event.type == pygame.MOUSEBUTTONDOWN: 
+                if input_box.collidepoint(event.pos): 
+                    text_box_clicked = True
+                elif train_box.collidepoint(event.pos):
+                    train()
+                else: 
+                    text_box_clicked = False
+    
+            if event.type == pygame.KEYDOWN: 
+                if text_box_clicked:
+                    if event.key == pygame.K_BACKSPACE: 
+                        # get text input from 0 to -1, excluding last character
+                        user_text = user_text[:-1] 
+                    elif event.key == pygame.K_RETURN:
+                        response = chatbot.response(user_text)
+                        user_text = ""
+                        spoke = True
+                        time = 0
+                    else: 
+                        user_text += event.unicode
+            
+            
+        screen.fill(colors["space cadet"])
+        input_box = pygame.Rect(50, 250, 400, 200)
+
+        if text_box_clicked:
+            pygame.draw.rect(screen, colors["bright pink"], input_box)
+        else:
+            pygame.draw.rect(screen, colors["pale dogwood"], input_box)
+
+        train_box = pygame.Rect(10, 10, 70, 30)
+        pygame.draw.rect(screen, colors["melon"], train_box)
+        text_train = base_font.render("Train!", True, colors["english violet"]) 
+        screen.blit(text_train, (train_box.x+5, train_box.y + 5))
+
+        #splits user text into lines of 35 characters each_text_arr = 
+        user_text_arr = format_text(user_text)
+
+        #renders user text
+        start_y = 5
+        for line in user_text_arr:
+            text_surface = base_font.render(line, True, (255, 255, 255)) 
+            # render at position stated in arguments 
+            screen.blit(text_surface, (input_box.x+5, input_box.y+start_y))
+            start_y += 18 
+
+        #splits response text into lines of 35 characters each
+        response_arr = format_text(response)
+
+        #renders response text
+        start_y_bot_offset = 40
+        for line in response_arr:
+            text_surface_bot = base_font.render(line, True, colors["fire brick"]) 
+            # render at position stated in arguments 
+            screen.blit(text_surface_bot, (25, 20+start_y_bot_offset))
+            start_y_bot_offset += 18 
+
+        if spoke and time + pygame.time.get_ticks() == 2000:
+            out = "say "
+            for char in response:
+                if char.isalpha() or char.isspace():
+                    out += char
+            os.system(out)
+            spoke = False
+        
+    
+        pygame.display.flip()
+
+    pygame.display.quit()
+    pygame.quit()
+
+#def q_and_a_mode():
+
+def train():
+    
+    screen = pygame.display.set_mode([500, 500])
+    running = True
+
+    base_font = pygame.font.Font(None, 32) 
+    user_text = ''
+    user_arr = []
+    text_box_clicked = False
+    question_entered = False
+    instructions = "Enter a question."
+
+    while running:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+            if event.type == pygame.MOUSEBUTTONDOWN: 
+                if input_box.collidepoint(event.pos): 
+                    text_box_clicked = True
+                elif train_box.collidepoint(event.pos):
+                    run_chatbot()
+                else: 
+                    text_box_clicked = False
+    
+            if event.type == pygame.KEYDOWN: 
+                if text_box_clicked:
+                    if event.key == pygame.K_BACKSPACE: 
+                        # get text input from 0 to -1, excluding last character
+                        user_text = user_text[:-1] 
+                    elif event.key == pygame.K_RETURN:
+                        if user_text == "-1":
+                            question_entered = False
+                            chatbot.add_to_csv(user_arr)
+                            user_arr = []
+                        else:
+                            user_arr += [user_text]
+                            if not question_entered:
+                                #if a question was just entered
+                                question_entered = True
+                        
+                        if question_entered:
+                            instructions = "Enter answer, or -1 to ask a new question."
+                        else:
+                            instructions = "Enter a question."
+                        user_text = ""
+                    else: 
+                        user_text += event.unicode
+
+        screen.fill(colors["space cadet"])
+        input_box = pygame.Rect(50, 250, 400, 200)
+        if text_box_clicked:
+            pygame.draw.rect(screen, colors["viridian"], input_box)
+        else:
+            pygame.draw.rect(screen, colors["castleton green"], input_box)
+
+        train_box = pygame.Rect(10, 10, 70, 30)
+        pygame.draw.rect(screen, colors["melon"], train_box)
+        text_train = base_font.render("Chat!", True, colors["english violet"]) 
+        screen.blit(text_train, (train_box.x+5, train_box.y + 5))
+
+        #splits user text into lines of 30 characters each
+        user_text_arr = format_text(user_text)
+
+        start_y = 5
+        for line in user_text_arr:
+            text_surface = base_font.render(line, True, (255, 255, 255)) 
+        
+            # render at position stated in arguments 
+            screen.blit(text_surface, (input_box.x+5, input_box.y+start_y))
+            start_y += 18 
+
+        start_y_bot_offset = 40
+        text_surface_bot = base_font.render(instructions, True, colors["fire brick"]) 
+        # render at position stated in arguments 
+        screen.blit(text_surface_bot, (25, 20+40))
+        start_y_bot_offset += 18 
+
+
+        pygame.display.flip()
+
+    pygame.display.quit()
+    pygame.quit()
+
+#returns a list of strings where each character is 35 characters long
+def format_text(text):
+    text = text.split(" ")
+    res = [""]
+
+    for string in text:
+        if len(res[-1]) + len(string) + 1 < 35:
+            res[-1] += (string + " ")
+        else:
+            res += [string + " "]
+    
+    return res
+    
+
+run_chatbot()
+
+
+
+  
+  
+
+  
+
+
+  
+    
+      
